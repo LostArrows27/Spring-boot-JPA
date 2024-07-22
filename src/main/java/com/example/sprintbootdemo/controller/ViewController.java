5835bb80958c6b7f6bde0abadabc87cc9793cf0b
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-07-22 15:27:20 +0700

    feat(view): add all users view + controller

--- src/main/java/com/example/sprintbootdemo/controller/ViewController.java
+++ src/main/java/com/example/sprintbootdemo/controller/ViewController.java
@@ -1,15 +1,28 @@
 package com.example.sprintbootdemo.controller;
 
+import com.example.sprintbootdemo.model.User;
+import com.example.sprintbootdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
 
-    @GetMapping("/test/thymeleaf")
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/test/thymeleaf")
     public String testThymeleaf(Model model) {
         model.addAttribute("message", "Hello, Thymeleaf !");
         return "index";
+    }
+
+    @RequestMapping("/test/users")
+    public String testUsers(Model model) {
+        User[] users = userService.getAllUsers().toArray(new User[0]);
+        model.addAttribute("users", users);
+        return "users";
     }
 }
