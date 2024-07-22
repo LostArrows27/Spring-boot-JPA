7bba5b080fa13fd43a105b948dd4395ddec53621
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-07-22 16:51:13 +0700

    feat(exception): handle user not found exception

--- src/main/java/com/example/sprintbootdemo/controller/UserController.java
+++ src/main/java/com/example/sprintbootdemo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.sprintbootdemo.controller;
 
+import com.example.sprintbootdemo.exception.UserNotFoundException;
 import com.example.sprintbootdemo.model.User;
 import com.example.sprintbootdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,12 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
-        return userService.getUserById(id);
+        User user =  userService.getUserById(id);
+        if (user == null) {
+            throw new UserNotFoundException("Not found user with ID: " + id);
+        }
+
+        return user;
     }
 
     @PostMapping
