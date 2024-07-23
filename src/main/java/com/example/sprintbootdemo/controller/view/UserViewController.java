f99589dac44ef4eccf8b78aa8648c96c029f708e
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-07-23 15:46:12 +0700

    feat(user): add redirect for /users/create GET end point

--- src/main/java/com/example/sprintbootdemo/controller/view/UserViewController.java
+++ src/main/java/com/example/sprintbootdemo/controller/view/UserViewController.java
@@ -3,6 +3,7 @@ package com.example.sprintbootdemo.controller.view;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class UserViewController {
@@ -10,5 +11,11 @@ public class UserViewController {
     @RequestMapping(value = "/createUser", method = RequestMethod.GET)
     public String createUserForm() {
         return "createUser";
+    }
+
+
+    @RequestMapping(value = "/users/create", method = RequestMethod.GET)
+    public RedirectView createUser() {
+        return new RedirectView("/createUser");
     }
 }
