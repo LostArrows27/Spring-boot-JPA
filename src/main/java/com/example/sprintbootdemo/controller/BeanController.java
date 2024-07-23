c17045014bacca1a20292a7f4e9e72db7863765f
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-07-23 14:50:39 +0700

    feat(req params): add request params to calculator bean

--- src/main/java/com/example/sprintbootdemo/controller/BeanController.java
+++ src/main/java/com/example/sprintbootdemo/controller/BeanController.java
@@ -1,19 +1,34 @@
 package com.example.sprintbootdemo.controller;
 
 import com.example.sprintbootdemo.component.Calculator;
+import com.example.sprintbootdemo.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BeanController {
 
-    @Autowired
-    private Calculator calculator;
 
-    @GetMapping("/bean/calculator/add")
-    public ResponseEntity<Integer> add() {
-        return ResponseEntity.ok(calculator.add(10, 5));
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @GetMapping(value = "/bean/calculator/add", params = {"a", "b"})
+    public ResponseEntity<Integer> add(
+            @RequestParam(value = "a") int a,
+            @RequestParam(value = "b") int b
+    ) {
+        return calculatorService.add(a, b);
     }
+
+    @GetMapping(value = "/bean/calculator/subtract", params = {"a", "b"})
+    public ResponseEntity<Integer> subtract(
+            @RequestParam(value = "a") int a,
+            @RequestParam(value = "b") int b
+    ) {
+        return calculatorService.subtract(a, b);
+    }
+
 }
